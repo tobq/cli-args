72cff8d3bf0a6c8ab753e527e3df70dc71694447
tobi <tobi.akiny@gmail.com>
2019-04-14 00:44:32 +0100

    altered error message

--- index.mjs
+++ index.mjs
@@ -21,11 +21,11 @@ function setArgument(arg, value) {
         if (!(key in currentArgs)) currentArgs[key] = {};
         currentArgs = currentArgs[key];
         if (typeof currentArgs !== "object")
-            throw new Error(`Illegal argument ${arg}
+            throw new Error(`Illegal argument '${arg}'
             Can not set option '${key}' of value '${currentArgs}'`)
     }
     const option = argKeys[lastIndex];
-    if (option in currentArgs) throw new Error(`Argument ${arg} has already been set`);
+    if (option in currentArgs) throw new Error(`Argument '${arg}' has already been set`);
     currentArgs[option] = value;
 }
 
@@ -34,9 +34,11 @@ while (i < pargs.length) {
     if (arg[0] === PRECEDER) {
         if (arg.length < 2) throw new Error(`Illegal empty argument '${arg}'`);
         if (arg[1] === PRECEDER) {
+            const option = arg.slice(2);
             i++;
-            if (i < pargs.length) setArgument(arg.slice(2), pargs[i]);
-            else throw new Error(`Missing value for argument '${arg}'`);
+            if (i < pargs.length) {
+                setArgument(option, pargs[i]);
+            } else throw new Error(`Missing value for argument '${option}'`);
         } else {
             setArgument(arg.slice(1), true);
         }
